16544ac26dd6db4de95672e90bf722a27fc2c6cb
rjvaldo <rivaldojoseguterres@gmail.com>
2022-08-08 01:44:48 +0700

    Update Grid

--- app/src/main/java/com/diary/klinikapp/GridActivity.java
+++ app/src/main/java/com/diary/klinikapp/GridActivity.java
@@ -1,14 +1,26 @@
 package com.diary.klinikapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GridActivity extends AppCompatActivity {
 
+    Button btnclicKat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid);
+
+        btnclicKat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(GridActivity.this, MarkaActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/diary/klinikapp/HomeActivity.java
+++ app/src/main/java/com/diary/klinikapp/HomeActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SubMenu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
 
