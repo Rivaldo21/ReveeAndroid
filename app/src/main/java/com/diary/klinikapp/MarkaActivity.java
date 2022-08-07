8c3cef67c0ba321f69563d861c5f08247f2d34c8
rjvaldo <rivaldojoseguterres@gmail.com>
2022-08-08 01:24:33 +0700

    Update Grid

---
+++ app/src/main/java/com/diary/klinikapp/MarkaActivity.java
@@ -0,0 +1,14 @@
+package com.diary.klinikapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MarkaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marka);
+    }
+}
