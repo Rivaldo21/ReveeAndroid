ff7ad680563b84b28a20bf171ed060bceb11caa4
rjvaldo <rivaldojoseguterres@gmail.com>
2022-08-10 04:34:05 +0700

    Update Drawable

--- app/src/main/java/com/diary/klinikapp/ContentItem.java
+++ app/src/main/java/com/diary/klinikapp/ContentItem.java
@@ -3,18 +3,21 @@ package com.diary.klinikapp;
 public class ContentItem {
 
     static int[] poster = {
-            R.drawable.posterone, R.drawable.postertwo, R.drawable.posterthree, R.drawable.posterfour, R.drawable.posterfive, R.drawable.six,
+            R.drawable.poster1, R.drawable.poster2, R.drawable.poster3, R.drawable.poster4, R.drawable.poster5, R.drawable.poster6,
+            R.drawable.poster7, R.drawable.poster8, R.drawable.poster9, R.drawable.poster10, R.drawable.poster11, R.drawable.poster12,
 
     };
 
     static String[] judul = {
-            "Sentru Ajuda", "Konaba Ami", "Metode Pagamentu", "Informasaun Klinika", "Termu & Kondisaun",
+            "Sentru Ajuda", "Konaba Ami", "Metode Pagamentu", "Informasaun Klinika", "Termu & Kondisaun","Termu & Kondisaun",
+            "Termu & Kondisaun","Termu & Kondisaun","Termu & Kondisaun","Termu & Kondisaun","Termu & Kondisaun","Termu & Kondisaun",
 
     };
 
     static String[] tahun = {
             "Metode Pagamento", "Konaba", "Mensajen",
             "Informasaun", "Sentru Ajuda",
+            "Horario Klinika","Horario Klinika","Horario Klinika","Horario Klinika","Horario Klinika","Horario Klinika",
             "Horario Klinika",
     };
 }
